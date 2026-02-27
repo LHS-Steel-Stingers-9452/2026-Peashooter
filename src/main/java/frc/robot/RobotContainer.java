4d3ea0b85d20a836d39cae25697ef4ebe7c862e6
Agweelurr <jimbobobjoe@proton.me>
2026-02-26 16:59:00 -0800

    Unfinished auto, but command set up

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -303,7 +303,10 @@ public class RobotContainer {
 
             SmartDashboard.putData("set hood from vision", hood.setPosition(visionpros::getHoodAngleFromTy));
 
+        //Named Commands, Necessary for autos!   NOT THE COMMANDS THEMSELVES, basically translating them to be used on pathplanner 
 
+
+            NamedCommands.registerCommand("Dump Mag", RobotContainer.dumpMag(shooter, indexer, kicker));
 
 
 
@@ -326,8 +329,12 @@ public class RobotContainer {
         );
     }
 
-    public Command shootFuel(){
-        return shooter.setVelocity(1.67); 
+    public static Command dumpMag(Shooter shooter, Indexer indexer, Kicker kicker){
+        return shooter.setVelocity(42)
+        .andThen(new WaitCommand(0.10))
+        .andThen(indexer.setVelocity(45).alongWith(kicker.setVelocity(25)))
+        .andThen(new WaitCommand(3))
+        .andThen(shooter.stopCommand().alongWith(kicker.stopCommand().alongWith(indexer.stopCommand())));
     }
 
     public Command intakeFuel(){
